deee8c2e01f02d899714bed7c537c38b6ef0b814
Guto Marcicano <caugmar@gmail.com>
2026-07-10 11:08:26 -0300

    Minha configuração do Doom Emacs.

---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326" "48cf035091998c18dd617a907cbc07effe917c5805621cb975a21979bd601c3d" default))
+ '(package-selected-packages '(flexoki-themes)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'customize-apropos 'disabled nil)
